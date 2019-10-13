9c076d27c251ed68c1502ca64d6bf27dcc137b20
Nadav <nadavbm@outlook.com>
2019-10-13 22:52:49 +0200

    accurate coordinates

--- geopy/flask-coordinate-inter/app/app.py
+++ geopy/flask-coordinate-inter/app/app.py
@@ -10,18 +10,6 @@ def index():
     redis.incr('hits')
     return render_template('index.html', visitor=redis.get('hits'))
 
-"""def interpreter():
-	if request.method == 'POST':
-		street = request.form.get("Street")
-	    number = request.form.get("Number")
-		city = request.form.get("City")
-		country = request.form.get("Country")
-		address = "%s, %s, %s, %s " % (street, number, city, country)
-	
-		geolocator = Nominatim(user_agent="Nadav")
-		location = geolocator.geocode("%s" % address)
-"""
-
 @app.route('/address',methods = ['POST', 'GET'])
 def result():
 	if request.method == 'POST':
@@ -29,7 +17,7 @@ def result():
 		addresstrans = "%s, %s, %s, %s " % ( request.form.get("Street"), request.form.get("Number"), request.form.get("City"), request.form.get("Country"))
 		geolocator = Nominatim(user_agent="Nadav")
 		location = geolocator.geocode("%s" % addresstrans)
-		coordinate = "%d, %d" % (location.latitude, location.longitude)
+		coordinate = "%.12f', %.12f" % (location.latitude, location.longitude)
 		return render_template("result.html",address = address, coordinate = coordinate)
 
 if __name__ == "__main__":
