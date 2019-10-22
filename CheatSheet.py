29eaf203dd6f4e7b7822935cf79280472207fcfc
Nadav Ben Mazia <nadav.benmazia@aeriagames.com>
2019-10-22 16:59:00 +0200

    Editing cheatsheet

--- CheatSheet.py
+++ CheatSheet.py
@@ -1,168 +1,62 @@
-""" One line comment start with #
+""" One line comment start with
+# This is a one line comment
 multi line with """
 
-# Import libraries:
-import string
-import math
+Modules:
+========
+import mod                                              # Call is mod.py and put it in the same directory as where app.py is running
+import string                                           # To import string (Search for module in this sequence: Current dir, PYTHONPATH, paths depeneded during python installation)
+from flask import flask, render_template, jsonfy        # from allows specific objects to be imported from module
+from flask_sqlalchemy import SQLAlchemy
+from <module_name> import <name> as <alt_name>          # use alternative name for an object in this .py file
+import <module_name> as <alt_name>
+import src.module, dir.module, pkg.module               # To use directory structure while running python - import a package
+from <package_name> import <module_name> as <alt_name>
 
-# Python functions:
-def main():
-    print("Hello World")
-
-main()
-
-# Using input and print:
-def vorname():
-    first_name = raw_input('Enter your name: ')
-    print("Hello " + first_name)
-
-vorname()
-
-# Python data-types: integer, float, complex, string, boolean
-# Variables:
-x = 3
+Variable and data-types:
+========================
+x = 3                                                   #Python data-types: integer, float, complex, string, boolean
 str = "Hello"
-# Define three variables at once:
-count, result, total = 0, 0, 0
-# Add 1 or minus 1:
-count += 1
-# All assignment operators: += , -= , *= , /= , %=
-# Determine the type of a variable:
-print(type(1))
-print(type("a"))
-print(type(2+3j))
+count, result, total = 0, 0, 0                          # Define three variables at once:
+count += 1                                              # Add 1 or minus 1, all assignment operators: += , -= , *= , /= , %=
 
-# Using strings:
-name = "Nadav"
-age = 35
-print("Hello! My name is %s." % name)
-print("Hello! My name is %s and I am %d years old." % (name, age))
-print('This is one line.\nThis is another line.')
-"""
-\\ literal backslash
-\' single quote
-\" double quote
-\n newline
-\t tab
-"""
-name = "Shipudei Afuli"
-print(len(name)) # Find the length of a string with the built-in len functio
-print(name.lower()) # Print the string converted to lowercase
-print(name.index("a"))
-print(name[0])
-print(name[3:5])
-# Converting integer to string:
-print("My number is " + str(3))
-print("My number is %d" % 3)
 # Using integers
 """ Integer operators:
 Addition: + ## Subtraction: - ## Multiplication: * ## Division: // ## Modulus (remainder): % ## Exponent (power): **
 The system will first handle brackets (), then **, then *, // and %, and finally + and -
 """
-# Global variable:
-a = 0
-def my_func():
-    print(a)
-my_func()
-# Modify global variable value:
-a = 0
-def my_func():
-    global a
-    a = 3
-    print(a)
-# my_func print 3:
-my_func()
 
-# Constants - Variable which can be assigned a value only once (once set - it cannot be changed):
-MAXIMUM_SPEED = 240
-MINMUM_SPEED = 0
+print(type(1))                                          # Determine the type of a variable
+print(type("a"))
+print(type(2+3j))
 
-# Use input as a variable:
-height = int(input("Enter height of rectangle: "))
+name = "Nadav"                                          # Using string variables
+age = 35
+print("Hello! My name is %s." % name)
+print("Hello! My name is %s and I am %d years old." % (name, age))
+print('This is one line.\nThis is another line.')       # Using strings -  \\ literal backslash    \' single quote      \" double quote     \n newline      \t tab
+
+name = "Shipudei Afuli"
+print(len(name))                                        # Find the length of a string with the built-in len functio
+print(name.lower())                                     # Print the string converted to lowercase
+print(name.index("a"))
+print(name[0])
+print(name[3:5])
+
+print("My number is " + str(3))                         # Converting integer to string
+print("My number is %d" % 3)
+
+height = int(input("Enter height of rectangle: "))      # Using input to assign variables
 width = int(input("Enter width of rectangle: "))
 print("The area of the rectangle is %d" % (width * height))
 
-# Convert to String - Casting:
-print("3%d" % 2) # will give 32
-print("3%d" % 2)
-print(int("3") + 8) # will give 11
+print("3%d" % 2)                                        # Casting - convert integer to string (This will output 32)
+print(int("3") + 8)                                     # will give 11
 print("3" + str(8))
 int(float("3.5"))
 
-# Selection control statements:
-if name == "Kebab":
-    print("Hello Kebab!")
-if (x > 4):
-    x -= 1
-# Using is or is not:
-a = "Kebab"
-b = "Kebab"
-if a is b:
-    print("Both a and b are Kebabs and both on fire")
-if a is not b:
-    print("Both Kebabs, but a cooked longer than b")
-# if else clause:
-if x < 3:
-    print("x = 3")
-else:
-    print("x != 3")
-# Nested if statement:
-kebabs = int(raw_input('how many kebabs would you like to eat? '))
-if kebabs <= 10:
-    if kebabs <= 3:
-        cost = 3*kebabs
-    else:
-        cost = 2*2*kebabs
-    print("Your dinner cost: %d" % cost)
-else:
-    print("You better go to all you can eat restaurant")
-# elif statement:
-if mark >= 80:
-    grade = A
-elif mark >= 65:
-    grade = B
-elif mark >= 50:
-    grade = C
-else:
-    grade = D
-# Boolean type:
-name = raw_input('Enter your name: ')
-if name:
-    print("Hello, %s!" % name)
-# The same as this:
-if bool(name) == True:
-    print("Hello, %s!" % name)
-# and operator:
-if (age >= 50 and age <= 70):
-    print("You should go to pension dude")
-# More and examples:
-if i < len(mylist) and mylist[i] == 3:
-    print("There is a value 3 in the list")
-if key in mydict and mydict[key] == 3:
-    print("There is a key equal to 3")
-# or operator:
-if age < 18 or age > 80:
-    print("You can't use our porn site in the age of %d" % age)
-# not operator
-name = raw_input('Enter your name: ')
-if not name.startswith("N"):
-    print("'%s' doesn't start with N!" % name)
-# more not operator:
-if not x == 5:
-    x += 1
-# conditional operator:
-result = "Pass" if (score >= 50) else "Fail"
-# There is no switch statements but however it could be acheived from dictionaries:
-NAMES = {
-"MA": "Moel Akol",
-"BA": "Bar Ashipudi",
-"SN": "Sar Netanyahu", # Trailing commas like this are allowed in Python!
-}
-if short in NAMES: # this tests whether the variable is one of the dictionary's keys
-    print("Name: %s" % NAMES[short])
-else:
-    print("Unknown shortcut: %s" % short)
-
+Arrays, Lists and Dictionaries:
+===============================
 # Lists:
 food = ['kebab', 'shipud', 'boesh', 'hagav']
 numbers = [13124, 3217, 343, 212, 1]
@@ -266,7 +160,84 @@ veg = list(menu)
 keb = tuple(menu.values())
 set = set(menu.items())
 
-# Loop control statements:
+
+Conditional statements:
+=======================
+if name == "Kebab":
+    print("Hello Kebab!")
+if (x > 4):
+    x -= 1
+# Using is or is not:
+a = "Kebab"
+b = "Kebab"
+if a is b:
+    print("Both a and b are Kebabs and both on fire")
+if a is not b:
+    print("Both Kebabs, but a cooked longer than b")
+# if else clause:
+if x < 3:
+    print("x = 3")
+else:
+    print("x != 3")
+# Nested if statement:
+kebabs = int(raw_input('how many kebabs would you like to eat? '))
+if kebabs <= 10:
+    if kebabs <= 3:
+        cost = 3*kebabs
+    else:
+        cost = 2*2*kebabs
+    print("Your dinner cost: %d" % cost)
+else:
+    print("You better go to all you can eat restaurant")
+# elif statement:
+if mark >= 80:
+    grade = A
+elif mark >= 65:
+    grade = B
+elif mark >= 50:
+    grade = C
+else:
+    grade = D
+# Boolean type:
+name = raw_input('Enter your name: ')
+if name:
+    print("Hello, %s!" % name)
+# The same as this:
+if bool(name) == True:
+    print("Hello, %s!" % name)
+# and operator:
+if (age >= 50 and age <= 70):
+    print("You should go to pension dude")
+# More and examples:
+if i < len(mylist) and mylist[i] == 3:
+    print("There is a value 3 in the list")
+if key in mydict and mydict[key] == 3:
+    print("There is a key equal to 3")
+# or operator:
+if age < 18 or age > 80:
+    print("You can't use our porn site in the age of %d" % age)
+# not operator
+name = raw_input('Enter your name: ')
+if not name.startswith("N"):
+    print("'%s' doesn't start with N!" % name)
+# more not operator:
+if not x == 5:
+    x += 1
+# conditional operator:
+result = "Pass" if (score >= 50) else "Fail"
+# There is no switch statements but however it could be acheived from dictionaries:
+NAMES = {
+"MA": "Moel Akol",
+"BA": "Bar Ashipudi",
+"SN": "Sar Netanyahu", # Trailing commas like this are allowed in Python!
+}
+if short in NAMES:                                                              # this tests whether the variable is one of the dictionary's keys
+    print("Name: %s" % NAMES[short])
+else:
+    print("Unknown shortcut: %s" % short)
+
+Loops:
+======
 # While loop - event-controlled loop statement. Initialisation -> Condition -> Update
 total = 0
 i = 1
@@ -357,3 +328,46 @@ def list_promptsum():
     print('the average of all floats is: %d' % (float(sum) // float(count)))
 
 list_promptsum()
+
+
+Function:
+=========
+def main():
+    print("Hello World")
+
+main()
+
+# Using input and print:
+def vorname():
+    first_name = raw_input('Enter your name: ')
+    print("Hello " + first_name)
+
+vorname()
+
+Constants and Global variables:
+===============================
+# Global variable:
+a = 0
+def my_func():
+    print(a)
+my_func()
+# Modify global variable value:
+a = 0
+def my_func():
+    global a
+    a = 3
+    print(a)
+# my_func print 3:
+my_func()
+
+# Constants - Variable which can be assigned a value only once (once set - it cannot be changed):
+MAXIMUM_SPEED = 240
+MINMUM_SPEED = 0
+
+
+
+
+
+
+
+# Loop control statements:
