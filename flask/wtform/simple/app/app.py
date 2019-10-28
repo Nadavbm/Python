07a7ad92593d9c60f76ef6de79c2e67a00ad6dfd
Nadav <nadavbm@outlook.com>
2019-10-28 22:08:45 +0100

    wtform with redirect to name variable in url

--- flask/wtform/simple/app/app.py
+++ flask/wtform/simple/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, redirect, render_template, flash, request, url_for
 from wtforms import Form, TextField, StringField, SubmitField, validators
 
 app = Flask(__name__)
@@ -8,18 +8,20 @@ app.config.from_object(__name__)
 class FormTemplate(Form):
 	name = TextField('Name', validators=[validators.required()])
 
+@app.route('/<string:name>')
+def profile(name):
+	return render_template('profile.html', name=name)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	form = FormTemplate(request.form)
 	if request.method == 'POST':
 		name = request.form['name']
-		print("Hello %s" % name)
-	if form.validate():
-		flash('Hello' + name)
-	else:
-		flash('All fields required!')
-
-		return render_template('index.html', form=form)
+		if form.validate():
+			return redirect(url_for('profile', name=request.form['name']))
+		else:
+			flash('All fields required!')
+	return render_template('index.html', form=form)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
